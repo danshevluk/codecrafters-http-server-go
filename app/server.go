1a17ac9f6700f8bb6f0645b62d7862be1f60e199
Dan Shevluk <dan.shevluk@hey.com>
2023-12-31 03:15:46 +0000

    Read path implementation

--- app/server.go
+++ app/server.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+const newline = "\r\n"
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -20,7 +23,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	readBuf := make([]byte, 1024)
+	_, err = conn.Read(readBuf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		os.Exit(1)
+	}
+	requestLines := strings.Split(string(readBuf), newline)
+	path := extractPath(&requestLines)
+
+	if path == "/" {
+		writeResponse(conn, "HTTP/1.1 200 OK"+newline+newline)
+	} else {
+		writeResponse(conn, "HTTP/1.1 404 Not Found"+newline+newline)
+	}
+}
+
+func extractPath(requestLines *[]string) string {
+	return strings.Split((*requestLines)[0], " ")[1]
+}
+
+func writeResponse(conn net.Conn, response string) {
+	_, err := conn.Write([]byte(response))
 	if err != nil {
 		fmt.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
